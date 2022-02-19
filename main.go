138ac5aa9f0da14706d5c8a20ab19309f78cf167
Raksix <ermagfurkan7@gmail.com>
2022-02-20 01:36:47 +0300

    https://blog.logrocket.com/how-to-build-blockchain-from-scratch-go/

--- main.go
+++ main.go
@@ -85,6 +85,17 @@ func (b Blockchain) isValid() bool {
 }
 
 func main() {
+
+	// create a new blockchain instance with a mining difficulty of 2
+	blockchain := CreateBlockchain(2)
+
+	// record transactions on the blockchain for Alice, Bob, and John
+	blockchain.addBlock("Alice", "Bob", 5)
+	blockchain.addBlock("John", "Bob", 2)
+
+	// check if the blockchain is valid; expecting true
+	fmt.Println(blockchain.isValid())
+
 	godotenv.Load(".env")
 	uri := os.Getenv("MONGO_URL")
 
