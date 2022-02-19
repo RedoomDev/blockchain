d0f4308c73d6b0b2543b66d8b9a988b0f1b4bb1f
Raksix <ermagfurkan7@gmail.com>
2022-02-20 01:30:29 +0300

    mongodb connection

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
-	"github.com/gin-gonic/gin"
-	"fmt"
+	"context"
+	"log"
 	"os"
-)
 
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
 	godotenv.Load(".env")
-	mongo := os.Getenv("MONGO_URL")
+	uri := os.Getenv("MONGO_URL")
+
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -18,5 +35,5 @@ func main() {
 			"message": "pong",
 		})
 	})
-		r.Run(":5000")
+	r.Run(":5000")
 }
